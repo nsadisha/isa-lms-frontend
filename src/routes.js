af82acfc2a0e947b2fc93c69f40714a284f9a55c
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-02 15:54:24 +0530

    initialize  /login and /register routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Login from './views/Login';
+import Register from './views/Register';
 
 const routes = createBrowserRouter([
     {
       path: "/",
       element: <h1>Hello from Router</h1>,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />
     }
   ]);
 
