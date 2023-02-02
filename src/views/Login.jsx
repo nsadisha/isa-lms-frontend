72e2567e01eefb8223fe7ff87d873f70ccd5c820
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-02 18:03:36 +0530

    handle login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,12 +1,19 @@
-import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import '../assets/css/login.scss'
+import { useEffect } from 'react';
+import AuthService from '../service/AuthService';
+import '../assets/css/login.scss';
 
 function hadndleLogin(e) {
     e.preventDefault()
+
     let email = e.target.email.value;
 	let password = e.target.password.value;
-	console.log(email, password);
+
+    AuthService.login(email, password).then(data => {
+        console.log(data);
+    }).catch(err => {
+        console.log(err);
+    })
 }
 
 function Login(props) {
@@ -21,11 +28,11 @@ function Login(props) {
                         <form onSubmit={hadndleLogin}>
                             <div className="mb-3">
                                 <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1" name='email' required />
+                                <input type="email" className="form-control" id="exampleInputEmail1" name='email' required value='nsadisha@gmail.com' />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1" name='password' />
+                                <input type="password" className="form-control" id="exampleInputPassword1" name='password' value='123123' />
                             </div>
                             <button type="submit" className="btn gradiant-btn w-100">Submit</button>
                         </form>
