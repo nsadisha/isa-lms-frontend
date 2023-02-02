2103275d5ab483910111d49f01f78eaf68e05045
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-02 15:25:48 +0530

    initialize router

---
+++ src/router.js
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <h1>Hello from Router</h1>,
+    }
+  ]);
+
+
+export default routes;
