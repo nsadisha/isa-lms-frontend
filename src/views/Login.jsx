24c53efae7dc7e7e41b36ad59e4277d0287aca1d
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-06 11:55:25 +0530

    removed hardcorded values

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
-import AuthService from '../service/AuthService';
+import authService from '../service/AuthService';
 import '../assets/css/login.scss';
 
 function hadndleLogin(e) {
@@ -9,7 +9,7 @@ function hadndleLogin(e) {
     let email = e.target.email.value;
 	let password = e.target.password.value;
 
-    AuthService.login(email, password).then(data => {
+    authService.login(email, password).then(data => {
         console.log(data);
     }).catch(err => {
         console.log(err);
@@ -20,19 +20,19 @@ function Login(props) {
     useEffect(() => {document.title = props.title;})
 
     return (
-        <div className="container login mt-5">
-            <div className="row justify-content-center">
+        <div className="container login">
+            <div className="row vh-100 justify-content-center align-items-center">
                 <div className="col-md-6 col-lg-4">
-                    <div>
+                    <div className='login-div'>
                         <h1 className='caption mb-5'>Login</h1>
                         <form onSubmit={hadndleLogin}>
                             <div className="mb-3">
                                 <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1" name='email' required value='nsadisha@gmail.com' />
+                                <input type="email" className="form-control" id="exampleInputEmail1" name='email' required />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1" name='password' value='123123' />
+                                <input type="password" className="form-control" id="exampleInputPassword1" name='password' />
                             </div>
                             <button type="submit" className="btn gradiant-btn w-100">Submit</button>
                         </form>
