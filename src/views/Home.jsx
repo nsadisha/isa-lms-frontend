b8f3fd8e10a8faa8f8fb1d311a5a043be8cd263b
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-06 17:53:00 +0530

    added nav bar

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,13 @@
+import NavBar from "../components/NavBar";
+
 function Home() {
-    return ( 
-        <h1>Hello from home.</h1>
-     );
+    
+	return (
+		<>
+            <NavBar />
+			<h1>Hello from home.</h1>
+		</>
+	);
 }
 
 export default Home;
