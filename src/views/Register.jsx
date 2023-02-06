ae6fa336c522f3a543129f34575f8d720d7fdf4d
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-06 13:15:18 +0530

    email validation and redirect after register

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,62 +1,85 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import authService from "../service/AuthService";
 
-let passwordValue = '';
-
-function handleRegister(e) {
-    e.preventDefault()
-    let data = createDataObjectForm(e.target)
-
-    if (!isPasswordValid(data.password) || !isConfirmPasswordValid(data.password, data.cPassword)) {
-        console.log("Error");
-        return;
-    }
-
-    authService.register(data).then(res => {
-        console.log(res);
-    }).catch(err => {
-        console.log(err);
-    })
-}
-
-function isPasswordValid(password) {
-    passwordValue = password;
-    return password.length > 7;
-}
-function isConfirmPasswordValid(password, cPassword) {
-    return cPassword === password
-}
-
-function handlePasswordField(e) {
-    if (isPasswordValid(e.target.value)) {
-        e.target.classList.remove('is-invalid')
-        e.target.classList.add('is-valid')
-    } else {
-        e.target.classList.add('is-invalid')
-    }
-}
-
-function handleConfirmPasswordField(e) {
-    if (isConfirmPasswordValid(passwordValue, e.target.value)) {
-        e.target.classList.remove('is-invalid')
-        e.target.classList.add('is-valid')
-    } else {
-        e.target.classList.add('is-invalid')
-    }
-}
-
-function createDataObjectForm(form) {
-    return {
-        first_name: form.fname.value,
-        last_name: form.lname.value,
-        email: form.email.value,
-        password: form.password.value,
-        cPassword: form.cPassword.value,
-        role: form.role.value
-    };
-}
-
 function Register() {
+    const navigate = useNavigate();
+    let passwordValue = '';
+    let duplicateEmailErrorMsg = 'This email address is already in use!';
+    let invalidEmailErrorMsg = 'Please enter a valid email address.';
+    const [emailErrMsg, setErrMsg] = useState(invalidEmailErrorMsg)
+
+    function handleRegister(e) {
+        e.preventDefault()
+        let data = createDataObjectForm(e.target)
+
+        if (!isPasswordValid(data.password) || !isConfirmPasswordValid(data.password, data.cPassword) || !isEmailValid(data.email)) {
+            console.log("Please check your fields again!");
+            return;
+        }
+        authService.register(data).then(res => {
+            console.log(res);
+            navigate('/login')
+        }).catch(err => {
+            setErrMsg(duplicateEmailErrorMsg)
+            e.target.email.classList.remove('is-valid')
+            e.target.email.classList.add('is-invalid')
+            console.log(err);
+        })
+    }
+
+    function isEmailValid(email) {
+        // eslint-disable-next-line
+        let regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,9})+$/
+        return regEx.test(email)
+    }
+    function isPasswordValid(password) {
+        passwordValue = password;
+        return password.length > 7;
+    }
+    function isConfirmPasswordValid(password, cPassword) {
+        return cPassword === password
+    }
+
+    const handlePasswordField = (e) => {
+        if (isPasswordValid(e.target.value)) {
+            e.target.classList.remove('is-invalid')
+            e.target.classList.add('is-valid')
+        } else {
+            e.target.classList.add('is-invalid')
+        }
+    }
+
+    function handleConfirmPasswordField(e) {
+        if (isConfirmPasswordValid(passwordValue, e.target.value)) {
+            e.target.classList.remove('is-invalid')
+            e.target.classList.add('is-valid')
+        } else {
+            e.target.classList.add('is-invalid')
+        }
+    }
+
+    function handleEmailField(e) {
+        setErrMsg(invalidEmailErrorMsg)
+        if (isEmailValid(e.target.value)) {
+            e.target.classList.remove('is-invalid')
+            e.target.classList.add('is-valid')
+        } else {
+            e.target.classList.add('is-invalid')
+        }
+    }
+
+    function createDataObjectForm(form) {
+        return {
+            first_name: form.fname.value,
+            last_name: form.lname.value,
+            email: form.email.value,
+            password: form.password.value,
+            cPassword: form.cPassword.value,
+            role: form.role.value
+        };
+    }
+
     return (
         <div className="container login">
             <div className="row vh-100 justify-content-center align-items-center">
@@ -74,7 +97,15 @@ function Register() {
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1" name='email' required />
+                                <input type="email"
+                                    className="form-control"
+                                    id="exampleInputEmail1"
+                                    name='email'
+                                    onChange={handleEmailField}
+                                    required />
+                                <div id="passwordFeedback" className="invalid-feedback">
+                                    {emailErrMsg}
+                                </div>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
@@ -83,7 +114,7 @@ function Register() {
                                     id="exampleInputPassword1"
                                     name='password'
                                     onChange={handlePasswordField} />
-                                <div id="validationServer04Feedback" className="invalid-feedback">
+                                <div id="passwordFeedback" className="invalid-feedback">
                                     Password must contain at least 8 characters.
                                 </div>
                             </div>
@@ -94,23 +125,23 @@ function Register() {
                                     id="exampleInputPassword1"
                                     name='cPassword'
                                     onChange={handleConfirmPasswordField} />
-                                <div id="validationServer04Feedback" className="invalid-feedback">
+                                <div id="cpasswordFeedback" className="invalid-feedback">
                                     Does not match with the password you entered!
                                 </div>
                             </div>
                             <div className="mb-3">
-                            <label className="form-label">Role</label><br />
+                                <label className="form-label">Role</label><br />
                                 <div className="form-check form-check-inline">
                                     <input className="form-check-input" type="radio" name="role" id="inlineRadio1" value="STUDENT" required />
-                                        <label className="form-check-label" htmlFor ="inlineRadio1">Student</label>
+                                    <label className="form-check-label" htmlFor="inlineRadio1">Student</label>
                                 </div>
                                 <div className="form-check form-check-inline">
                                     <input className="form-check-input" type="radio" name="role" id="inlineRadio2" value="TEACHER" required />
-                                        <label className="form-check-label" htmlFor ="inlineRadio2">Teacher</label>
+                                    <label className="form-check-label" htmlFor="inlineRadio2">Teacher</label>
                                 </div>
                                 <div className="form-check form-check-inline">
                                     <input className="form-check-input" type="radio" name="role" id="inlineRadio3" value="MANAGEMENT_STAFF" required />
-                                        <label className="form-check-label" htmlFor ="inlineRadio3">Management Staff</label>
+                                    <label className="form-check-label" htmlFor="inlineRadio3">Management Staff</label>
                                 </div>
                             </div>
                             <button type="submit" className="btn gradiant-btn w-100">Submit</button>
