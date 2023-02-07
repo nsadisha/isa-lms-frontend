effe34bc085b25440addf9f2c6ef47836722fa36
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-07 16:56:28 +0530

    display relevent component depending on the signed state

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,37 @@
-import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
+import localStorageService from "../service/LocalStorageService";
+import userService from "../service/UserService";
 
-function Home() {
+import NavBar from "../components/NavBar";
+import AuthenticatedHome from "../components/authenticated/AuthenticatedHome";
+import AnnonymousHome from "../components/non_authenticated/AnnonymousHome";
+
+function Home({title}) {
+    const isSigned = localStorageService.isSigned()
+    const [userData, setUserData] = useState({});
+
     
+    useEffect(() => {
+        if(isSigned){
+            const token = localStorageService.getToken();
+            userService.getCurrentUserInfo(token)
+            .then(data => {
+                setUserData(data);
+            }).catch(err => {
+                console.log(err);
+            })
+        }
+        document.title = title;
+    }, [isSigned, title])
+
 	return (
 		<>
-            <NavBar />
-			<h1>Hello from home.</h1>
+            <NavBar isSigned={isSigned}/>
+			{
+                isSigned ? 
+                <AuthenticatedHome data={userData} /> : 
+                <AnnonymousHome />
+            }
 		</>
 	);
 }
