128e15924cecd72e57e2b53fdfa3c966c594e7fc
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-08 17:21:40 +0530

    padding top after navbar

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,22 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import ManagementStaffDashboard from "../components/authenticated/ManagementStaffDashboard";
-import StudentDashboard from "../components/authenticated/StudentDashboard";
-import TeacherDashboard from "../components/authenticated/TeacherDashboard";
+import ManagementStaffDashboard from "../components/dashboard/management/ManagementStaffDashboard";
+import StudentDashboard from "../components/dashboard/student/StudentDashboard";
+import TeacherDashboard from "../components/dashboard/teacher/TeacherDashboard";
 import NavBar from "../components/NavBar";
 import localStorageService from "../service/LocalStorageService";
 
 function Dashboard({title}) {
     const navigate = useNavigate();
     const isSigned = localStorageService.isSigned();
+    let token = localStorageService.getToken();
     const role = localStorageService.getRole();
 
     useEffect(() => {
         if(!isSigned){
             navigate('/')
         }
-
-        const token = localStorageService.getToken();
 
         document.title = title
     }, [title, navigate, isSigned])
@@ -25,9 +24,11 @@ function Dashboard({title}) {
         <>
             <NavBar isSigned={isSigned} />
 
-            {role==="MANAGEMENT_STAFF" && <ManagementStaffDashboard />}
-            {role==="TEACHER" && <TeacherDashboard />}
-            {role==="STUDENT" && <StudentDashboard />}
+            <div className="mt-5">
+                {role==="MANAGEMENT_STAFF" && <ManagementStaffDashboard token={token} />}
+                {role==="TEACHER" && <TeacherDashboard token={token} />}
+                {role==="STUDENT" && <StudentDashboard token={token} />}
+            </div>
         </>
     );
 }
