2524c30c1903152d1c424c6268c93a8fe0ed9a59
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-08 14:49:05 +0530

    tab related components

---
+++ src/components/tab/TabPane.jsx
@@ -0,0 +1,8 @@
+function TabPane({active, id, children}) {
+    let activeClass = active==='true' ? 'active' : '';
+    return ( 
+        <div className={"tab-pane fade show "+activeClass} id={id} role="tabpanel">{children}</div>
+    );
+}
+
+export default TabPane;
