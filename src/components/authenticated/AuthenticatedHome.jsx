3ad7ed888aefa1907c451147f3f57827e146a929
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-08 11:00:08 +0530

    Alert does not show after closed

--- src/components/authenticated/AuthenticatedHome.jsx
+++ src/components/authenticated/AuthenticatedHome.jsx
@@ -1,13 +1,13 @@
 import Alert from "../Alert";
 
-function AuthenticatedHome({data}) {
+function AuthenticatedHome({data, isFirstTimeAfterSigned}) {
 
     return ( 
         <>
             <div className="container">
                 <div className="row justify-content-center py-3">
                     <div className="col-md-6">
-                        <Alert message={"Welcome back "+data.first_name+"!"} />
+                        {isFirstTimeAfterSigned && <Alert message={"Welcome back "+data.first_name+"!"} />}
                     </div>
                 </div>
             </div>
