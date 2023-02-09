67d5d0cc787f46e7128615f4ab8b183328816376
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-09 11:55:31 +0530

    Empty state added fror payments section

--- src/components/dashboard/management/ManagementStaffDashboard.jsx
+++ src/components/dashboard/management/ManagementStaffDashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useCallback } from "react";
 import managementStaffService from "../../../service/ManagementStaffService";
+import EmptyState from "../../EmptyState";
 import TabItem from "../../tab/TabItem";
 import TabItemGroup from "../../tab/TabItemGroup";
 import TabPane from "../../tab/TabPane";
@@ -30,7 +31,6 @@ function ManagementStaffDashboard({token}) {
                 <div className="col-md-3">
                     <TabItemGroup>
                         <TabItem target='#home-tab' title='Home' active='true' />
-                        <TabItem target='#stats-tab' title='Statistics' />
                         <TabItem target='#all-users-tab' title='All users' />
                         <TabItem target='#payments-tab' title='Payments' />
                         <TabItem target='#settings-tab' title='Settings' />
@@ -39,12 +39,14 @@ function ManagementStaffDashboard({token}) {
                 <div className="col-md-9">
                     <TabPaneGroup id='management-dashboard-tabs'>
                         <TabPane id='home-tab' active='true'><h2>Home</h2></TabPane>
-                        <TabPane id='stats-tab'><h2>Statistics</h2></TabPane>
                         <TabPane id='all-users-tab'>
                             <h2 className="mb-3">All users</h2><hr className="text-secondary" />
                             <AllUsers users={users} />
                         </TabPane>
-                        <TabPane id='payments-tab'><h2>Payments</h2></TabPane>
+                        <TabPane id='payments-tab'>
+                            <h2>Payments</h2>
+                            <EmptyState message='No payment related information.' />
+                        </TabPane>
                         <TabPane id='settings-tab'>
                             <h2 className="mb-3">Settings</h2><hr className="text-secondary" />
                             <Settings token={token} callback={updateUsers}/>
