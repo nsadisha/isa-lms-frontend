cb4f97a47203a976c5e2e2b53b58b0d024c74e34
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-13 16:47:58 +0530

    alert close issue fixed

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,16 +1,18 @@
+import { useState } from "react";
 import localStorageService from "../service/LocalStorageService";
 
 function Alert({message, varient='primary'}) {
-    function handleAlertClose() {
-        localStorageService.setIsFirstTime(false);
-    }
-
-    const styles = {
+    const [styles, setStyles] = useState({
         position: 'absolute',
         transform: 'translateX(-50%)',
         left: '50%',
         top: '15%'
-    };
+    });
+
+    function handleAlertClose() {
+        setStyles({...styles, display: 'none'})
+        localStorageService.setIsFirstTime(false);
+    }
 
     return (
         <div className={"alert alert-"+varient+" alert-dismissible"} style={styles}>
@@ -18,8 +20,8 @@ function Alert({message, varient='primary'}) {
             <button 
                 type="button"
                 className="btn-close"
-                data-bs-dismiss="alert"
-                aria-label="Close"
+                // data-bs-dismiss="alert"
+                // aria-label="Close"
                 onClick={handleAlertClose}></button>
         </div>
 
