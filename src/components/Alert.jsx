04e1d220901897d536856d3848a99c5f24577592
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-13 12:50:03 +0530

    make position absolute

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -5,9 +5,16 @@ function Alert({message, varient='primary'}) {
         localStorageService.setIsFirstTime(false);
     }
 
+    const styles = {
+        position: 'absolute',
+        transform: 'translateX(-50%)',
+        left: '50%',
+        top: '15%'
+    };
+
     return (
-        <div className={"alert alert-"+varient+" alert-dismissible"} role="alert">
-            <div>{message}</div>
+        <div className={"alert alert-"+varient+" alert-dismissible"} style={styles}>
+            <div className="me-3">{message}</div>
             <button 
                 type="button"
                 className="btn-close"
