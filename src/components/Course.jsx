2011dc7b69d845b5919203b966aea1df4ac21fef
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-24 09:59:16 +0530

    fix issue with course title

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -8,7 +8,7 @@ function Course({course, col=3}) {
                 <div className="image">
                     <img src={courseSvg} alt="course svg" />
                 </div>
-                <div className="mx-3 mt-4">
+                <div className="mx-3 mt-4 d-grid">
                     <h5 className="name mb-1">{course.name}</h5>
                     <p className="code"><strong>{course.course_code}</strong></p>
                 </div>
