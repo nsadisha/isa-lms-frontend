2d9953dd47df76e2294f883ad93ffaf7ddecbd25
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-27 17:25:03 +0530

    display enrolled and latest(4) courses

--- src/components/home/StudentHome.jsx
+++ src/components/home/StudentHome.jsx
@@ -1,7 +1,37 @@
-import banner from "../../assets/img/student-banner.jpg"
+import banner from "../../assets/img/student-banner.jpg";
+import Course from "../Course";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import courseService from "../../service/CourseService";
+import studentService from "../../service/StudentService";
+import localStorageService from "../../service/LocalStorageService";
 
 function StudentHome() {
+    const token = localStorageService.getToken();
+    const [latestCourses, setLatestCourses] = useState([]);
+    const [enrolledCourses, setEnrolledCourses] = useState([]);
+
+    useEffect(() => {
+        courseService.getAllCourses()
+        .then(res => {
+            let size = res.length>4 ? res.length - 4 : 4;
+            setLatestCourses(res.slice(size).reverse());
+        }).catch(err => {
+            console.log(err);
+        });
+        
+        studentService.getEnrolledCourses(token)
+        .then(res => {
+            let size = res.length>4 ? res.length - 4 : 0;
+            setEnrolledCourses(res.slice(size).reverse())
+        }).catch(err => {
+            console.log(err
+            );
+        });
+
+        studentService.getEnrolledCourses()
+    }, [token, setLatestCourses, setEnrolledCourses])
+
     return (
         <>
             <div className="container-fluid page-banner" style={{ background: `url(${banner}) rgba(0,0,0,0.4)` }}>
@@ -14,6 +44,34 @@ function StudentHome() {
                                 <button className="gradiant-btn r-50 px-5">Explore more</button>
                             </Link>
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container mt-5">
+                <div className="row">
+                    <h2 className="w-100 mb-4">Latest courses</h2>
+                    {latestCourses.map(course => {
+                        return <Course course={course} key={course.id} />
+                    })}
+                    <div className="col-md-12 py-3 justify-content-center d-flex">
+                        <Link to="/">
+                            <button className="gradiant-btn r-50 px-5">View all courses</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container mt-5">
+                <div className="row">
+                    <h2 className="w-100 mb-4">Enrolled courses</h2>
+                    {enrolledCourses.map(course => {
+                        return <Course course={course} key={course.id} />
+                    })}
+                    <div className="col-md-12 py-3 justify-content-center d-flex">
+                        <Link to="/">
+                            <button className="gradiant-btn r-50 px-5">View enrolled courses</button>
+                        </Link>
                     </div>
                 </div>
             </div>
