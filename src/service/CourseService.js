40b5e7b1129c90d3276c1b56efc72c53d2327879
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-27 09:56:15 +0530

    filter course conductor properties

--- src/service/CourseService.js
+++ src/service/CourseService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { filterUserProperties } from "./FormHelperService";
 
 class CourseService {
     async getCourseDetails(token, courseId) {
@@ -7,6 +8,7 @@ class CourseService {
                 Authorization: `Bearer ${token}`
             }
         }).then(res => {
+            res.data.conductor = filterUserProperties(res.data.conductor);
             return res.data;
         }).catch(err => {
             throw err;
