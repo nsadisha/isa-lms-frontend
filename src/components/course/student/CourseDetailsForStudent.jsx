797b44a2e28662be67e2bf00e72e5ce4a354a0ab
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-27 10:20:22 +0530

    course details view for students

---
+++ src/components/course/student/CourseDetailsForStudent.jsx
@@ -0,0 +1,11 @@
+function CourseDetailsForStudent({ course }) {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">For students</div>
+            </div>
+        </div>
+    );
+}
+
+export default CourseDetailsForStudent;
