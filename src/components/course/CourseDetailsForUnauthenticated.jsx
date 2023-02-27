8e03f1e42594d3e1a4be27eb84ff4e0bb782ba0b
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-27 10:19:42 +0530

    course details view for everyone

---
+++ src/components/course/CourseDetailsForUnauthenticated.jsx
@@ -0,0 +1,13 @@
+function CourseDetailsForUnauthenticated({ course }) {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    For everyone
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CourseDetailsForUnauthenticated;
