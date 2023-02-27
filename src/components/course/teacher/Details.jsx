be58581a01498e32a963ece697b17c151c961aa4
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-27 11:50:36 +0530

    check for empty descriptions

--- src/components/course/teacher/Details.jsx
+++ src/components/course/teacher/Details.jsx
@@ -1,7 +1,7 @@
 function Details({ course }) {
     //return if the details are not loaded yet.
     if(course.conductor === undefined) return;
-    
+
     return (
         <div className="row justify-content-center mt-3">
             <div className="col-md-12 pt-3">
@@ -10,7 +10,7 @@ function Details({ course }) {
                 <h4>Course name: <span className="text-secondary">{course.name}</span></h4>
                 <h4>Course code: <span className="text-secondary">{course.course_code}</span></h4>
                 <h4>Description:</h4>
-                <p>{course.description}</p>
+                <p>{course.description.length===0 ? "This course does not have any description." : course.description}</p>
 
                 <div className="my-5"></div>
 
