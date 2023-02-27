238535fd2c9fc383d4c4959d72d1a2694e440684
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-27 11:00:50 +0530

    load necessary data to the component

--- src/components/course/teacher/CourseDetailsForTeacher.jsx
+++ src/components/course/teacher/CourseDetailsForTeacher.jsx
@@ -1,16 +1,35 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import courseService from "../../../service/CourseService";
+import localStorageService from "../../../service/LocalStorageService";
 import NavDropdownItem from "../../nav/NavDropdownItem";
 import NavItem from "../../nav/NavItem";
 import NavItemGroup from "../../nav/NavItemGroup";
 import NavPane from "../../nav/NavPane";
 import NavPaneGroup from "../../nav/NavPaneGroup";
+import Activities from "./Activities";
+import Details from "./Details";
+import Participants from "./Participants";
 
-function CourseDetailsForTeacher({course}) {
+function CourseDetailsForTeacher({ course }) {
+    const token = localStorageService.getToken();
+    const [participants, setParticipants] = useState([]);
+
+    useEffect(() => {
+        if (course.id === undefined) return;
+        courseService.getEnrolledStudents(token, course.id)
+            .then(res => {
+                setParticipants(res);
+            }).catch(err => {
+                console.log(err);
+            })
+    }, [token, course]);
+
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-12">
-                    <h1>Course id: {course.name}</h1>
+                    <h2 className="mb-4">{course.course_code} - {course.name}</h2>
                     <NavItemGroup>
                         <NavItem title="Activities" target="#nav-activities" active="true" />
                         <NavItem title="Participants" target="#nav-participants" />
@@ -21,9 +40,15 @@ function CourseDetailsForTeacher({course}) {
                         </NavDropdownItem>
                     </NavItemGroup>
                     <NavPaneGroup>
-                        <NavPane id="nav-activities" active="true">Activities</NavPane>
-                        <NavPane id="nav-participants">Participants</NavPane>
-                        <NavPane id="nav-details">Details</NavPane>
+                        <NavPane id="nav-activities" active="true">
+                            <Activities />
+                        </NavPane>
+                        <NavPane id="nav-participants">
+                            <Participants participants={participants} />
+                        </NavPane>
+                        <NavPane id="nav-details">
+                            <Details course={course} />
+                        </NavPane>
                     </NavPaneGroup>
                 </div>
             </div>
