73c573f8fb568f53e4a5069375dd40cc7d5efc0f
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-27 11:26:29 +0530

    show course details

---
+++ src/components/course/teacher/Details.jsx
@@ -0,0 +1,26 @@
+function Details({ course }) {
+    //return if the details are not loaded yet.
+    if(course.conductor === undefined) return;
+    
+    return (
+        <div className="row justify-content-center mt-3">
+            <div className="col-md-12 pt-3">
+                <h3>Course details</h3>
+                <hr className="mb-4" />
+                <h4>Course name: <span className="text-secondary">{course.name}</span></h4>
+                <h4>Course code: <span className="text-secondary">{course.course_code}</span></h4>
+                <h4>Description:</h4>
+                <p>{course.description}</p>
+
+                <div className="my-5"></div>
+
+                <h3>Conductor details</h3>
+                <hr className="mb-4" />
+                <h4>Name: <span className="text-secondary">{course.conductor.first_name} {course.conductor.last_name}</span></h4>
+                <h4>Email: <span className="text-secondary">{course.conductor.email}</span></h4>
+            </div>
+        </div>
+    );
+}
+
+export default Details;
