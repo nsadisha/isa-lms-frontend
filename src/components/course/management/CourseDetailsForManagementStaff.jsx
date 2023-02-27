da38a604952740d25d5a5ef789a4038c023b4d04
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-27 10:20:05 +0530

    course details view for management staff members

---
+++ src/components/course/management/CourseDetailsForManagementStaff.jsx
@@ -0,0 +1,13 @@
+function CourseDetailsForManagementStaff({ course }) {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    for mgt
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CourseDetailsForManagementStaff;
