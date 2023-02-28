243481c0e72bf87b2d9041aa0a231466d8d94321
Sadisha Nimsara <nsadisha@gmail.com>
2023-02-28 15:12:29 +0530

    show index instead of id

--- src/components/course/Participants.jsx
+++ src/components/course/Participants.jsx
@@ -19,10 +19,10 @@ function Participants({ participants }) {
                         </tr>
                     </thead>
                     <tbody>
-                        {participants.map(user => {
+                        {participants.map((user, index) => {
                             return (
                                 <tr key={user.email}>
-                                    <th scope="row">{user.id}</th>
+                                    <th scope="row">{index+1}</th>
                                     <td>{user.first_name + " " + user.last_name}</td>
                                     <td>{user.email}</td>
                                     <td>{user.role}</td>
